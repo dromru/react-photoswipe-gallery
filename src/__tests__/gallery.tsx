a989e7f2c881b08cf93f57612391976d7b7d1425
Ivan Rudoy <rudoy@farpost.com>
2020-04-23 05:31:22 +1000

    test: configure Jest, write some test cases
    closes #7

---
+++ src/__tests__/gallery.tsx
@@ -0,0 +1,173 @@
+import React, { useRef } from 'react'
+import PhotoSwipe from 'photoswipe'
+import { mount, shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import { Gallery, GalleryProps, Item, DefaultLayout, LayoutProps } from '..'
+
+const PhotoSwipeMocked = PhotoSwipe as jest.MockedClass<typeof PhotoSwipe>
+
+jest.mock('photoswipe', () => {
+  return jest.fn().mockImplementation(() => {
+    return { init: () => {} }
+  })
+})
+
+beforeEach(() => PhotoSwipeMocked.mockClear())
+
+function shuffle<T>(array: T[]) {
+  const result = [...array]
+  let currentIndex = result.length
+  let temp: any
+  let randomIndex: number
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+    temp = result[currentIndex]
+    // eslint-disable-next-line no-param-reassign
+    result[currentIndex] = result[randomIndex]
+    // eslint-disable-next-line no-param-reassign
+    result[randomIndex] = temp
+  }
+
+  return result
+}
+
+interface ImageItem {
+  original: string
+  thumbnail: string
+  width: number
+  height: number
+  title: string
+}
+
+const photoswipeArgsMock = (items: ImageItem[], index: number) => [
+  expect.anything(),
+  expect.anything(),
+  items.map(({ original, thumbnail, width, height, title }) => ({
+    src: original,
+    msrc: thumbnail,
+    w: width,
+    h: height,
+    title,
+    thumbEl: expect.anything(),
+  })),
+  {
+    getThumbBoundsFn: expect.anything(),
+    index,
+  },
+]
+
+const createItem = (index: number): ImageItem => ({
+  original: `https://placekitten.com/1024/768?image=${index}`,
+  thumbnail: `https://placekitten.com/160/120?image=${index}`,
+  width: 1024,
+  height: 768,
+  title: `kitty #${index}`,
+})
+
+const createItems = (length: number): ImageItem[] =>
+  Array.from({ length }, (_, i) => createItem(i))
+
+const TestGallery: React.FC<{ items: ImageItem[] } & GalleryProps> = ({
+  items,
+  ...rest
+}) => (
+  <Gallery {...rest}>
+    {items.map(({ original, thumbnail, width, height, title }, i) => (
+      <Item
+        key={original}
+        original={original}
+        thumbnail={thumbnail}
+        width={width}
+        height={height}
+        title={title}
+      >
+        {({ open, thumbnailRef }) => (
+          <img onClick={open} src={thumbnail} ref={thumbnailRef} />
+        )}
+      </Item>
+    ))}
+  </Gallery>
+)
+
+const TestGalleryWithLayout: React.FC<{ items: ImageItem[] } & LayoutProps> = ({
+  items,
+  ...rest
+}) => {
+  const layoutRef = useRef()
+  return (
+    <>
+      <TestGallery items={items} layoutRef={layoutRef} />
+      <DefaultLayout ref={layoutRef} {...rest} />
+    </>
+  )
+}
+
+describe('gallery', () => {
+  test('item click should init photoswipe', () => {
+    const items = createItems(3)
+    const wrapper = mount(<TestGallery items={items} />)
+
+    wrapper.find(Item).first().simulate('click')
+    expect(PhotoSwipeMocked).toHaveBeenCalledWith(
+      ...photoswipeArgsMock(items, 0),
+    )
+
+    wrapper.find(Item).last().simulate('click')
+    expect(PhotoSwipeMocked).toHaveBeenCalledWith(
+      ...photoswipeArgsMock(items, 2),
+    )
+  })
+
+  test('add one, then item click should init photoswipe', () => {
+    const items = createItems(3)
+    const wrapper = mount(<TestGallery items={items} />)
+
+    const newItems = [createItem(items.length + 1), ...items]
+
+    wrapper.setProps({ items: newItems })
+
+    wrapper.find(Item).first().simulate('click')
+    expect(PhotoSwipeMocked).toHaveBeenCalledWith(
+      ...photoswipeArgsMock(newItems, 0),
+    )
+
+    wrapper.find(Item).last().simulate('click')
+    expect(PhotoSwipeMocked).toHaveBeenCalledWith(
+      ...photoswipeArgsMock(newItems, 3),
+    )
+  })
+
+  test('shuffle, then item click should init photoswipe', () => {
+    const items = createItems(20)
+    const wrapper = mount(<TestGallery items={items} />)
+
+    const newItems = shuffle(items)
+
+    wrapper.find(Item).at(5).simulate('click')
+    expect(PhotoSwipeMocked).toHaveBeenCalledWith(
+      ...photoswipeArgsMock(items, 5),
+    )
+
+    wrapper.setProps({ items: newItems })
+
+    wrapper.find(Item).at(10).simulate('click')
+    expect(PhotoSwipeMocked).toHaveBeenCalledWith(
+      ...photoswipeArgsMock(newItems, 10),
+    )
+    wrapper.find(Item).first().simulate('click')
+    expect(PhotoSwipeMocked).toHaveBeenCalledWith(
+      ...photoswipeArgsMock(newItems, 0),
+    )
+    wrapper.find(Item).last().simulate('click')
+    expect(PhotoSwipeMocked).toHaveBeenCalledWith(
+      ...photoswipeArgsMock(newItems, 19),
+    )
+  })
+
+  test('should render with external layout', () => {
+    const items = createItems(1)
+    const wrapper = shallow(<TestGalleryWithLayout items={items} />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+})
