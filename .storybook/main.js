f1273023b9c58474ee9ffcf21da9bacf2d7e5f2a
Ivan Rudoy <rudoy@farpost.com>
2021-04-10 20:12:33 +1000

    chore: bump storybook; use builder: webpack5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,9 @@
 module.exports = {
   stories: ['../src/**/*.stories.@(ts|tsx)'],
   addons: ['@storybook/addon-knobs/register', '@storybook/addon-storysource'],
+  core: {
+    builder: 'webpack5',
+  },
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
