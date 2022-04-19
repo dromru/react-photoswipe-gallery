df5bb0bea9d8481cab17d38e8b9dbcbef8d3f5e4
Ivan Rudoy <rudoy@farpost.com>
2022-04-19 17:45:46 +1000

    docs(storybook): use short sha

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,11 +2,13 @@ import { themes } from '@storybook/theming'
 
 const currentRef = encodeURIComponent(process.env.GITHUB_REF_NAME || 'local')
 const currentSha = process.env.GITHUB_SHA
+const shortSha = currentSha ? currentSha.substring(0, 7) : ''
+
 const brandUrlTail = currentRef === 'local' ? '' : `tree/${currentRef}`
 const brandSettings = {
-  brandTitle: `react-photoswipe-gallery/${currentRef} - Storybook${currentSha ? ` - ${currentSha}` : ''}`,
+  brandTitle: `react-photoswipe-gallery/${currentRef} - Storybook${shortSha ? ` - ${shortSha}` : ''}`,
   brandUrl: `https://github.com/dromru/react-photoswipe-gallery/${brandUrlTail}`,
-  brandImage: `https://badgen.net/badge/icon/${currentSha || 'local'}?icon=github&label=${currentRef}`,
+  brandImage: `https://badgen.net/badge/icon/${shortSha || 'local'}?icon=github&label=${currentRef}`,
 }
 
 export const parameters = {
