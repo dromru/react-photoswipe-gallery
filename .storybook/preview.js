09a81192a19ac82f9cbff2ebff430a26eceadf63
Ivan Rudoy <rudoy@farpost.com>
2022-04-19 16:28:30 +1000

    docs(storybook): show current ref

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,20 @@
+import { themes } from '@storybook/theming'
+
+const currentRef = encodeURIComponent(process.env.GITHUB_REF_SLUG || 'local')
+const brandUrlTail = currentRef === 'local' ? '' : `tree/${currentRef}`
+
 export const parameters = {
   controls: { hideNoControlsWarning: true },
   darkMode: {
     stylePreview: true,
     darkClass: 'lights-out',
     lightClass: 'lights-on',
+    dark: {
+      ...themes.dark,
+      brandTitle: `react-photoswipe-gallery/${currentRef} - Storybook`,
+      brandUrl: `https://github.com/dromru/react-photoswipe-gallery/${brandUrlTail}`,
+      brandImage: `https://badgen.net/badge/icon/${currentRef}?icon=github&label`,
+    },
   },
   options: {
     storySort: {
