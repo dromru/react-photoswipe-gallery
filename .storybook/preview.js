7e034946e7a8f24475a366b2739e5ae360f29e82
Ivan Rudoy <rudoy@farpost.com>
2022-04-19 16:39:12 +1000

    docs(storybook): expose brand settings to the `light` theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,11 @@ import { themes } from '@storybook/theming'
 
 const currentRef = encodeURIComponent(process.env.GITHUB_REF_SLUG || 'local')
 const brandUrlTail = currentRef === 'local' ? '' : `tree/${currentRef}`
+const brandSettings = {
+  brandTitle: `react-photoswipe-gallery/${currentRef} - Storybook`,
+  brandUrl: `https://github.com/dromru/react-photoswipe-gallery/${brandUrlTail}`,
+  brandImage: `https://badgen.net/badge/icon/${currentRef}?icon=github&label`,
+}
 
 export const parameters = {
   controls: { hideNoControlsWarning: true },
@@ -11,9 +16,11 @@ export const parameters = {
     lightClass: 'lights-on',
     dark: {
       ...themes.dark,
-      brandTitle: `react-photoswipe-gallery/${currentRef} - Storybook`,
-      brandUrl: `https://github.com/dromru/react-photoswipe-gallery/${brandUrlTail}`,
-      brandImage: `https://badgen.net/badge/icon/${currentRef}?icon=github&label`,
+      ...brandSettings,
+    },
+    light: {
+      ...themes.light,
+      ...brandSettings,
     },
   },
   options: {
