8bc42f6df5a500805990ce935ecb8b04bf6ad408
Alexander Pepper <alexander.pepper@justrelate.com>
2022-08-03 09:58:25 +0200

    Fixed typo in "no-ref-error.ts" ("shuld" should be "should")

--- src/no-ref-error.ts
+++ src/no-ref-error.ts
@@ -1,7 +1,7 @@
 export class NoRefError extends Error {
   /* istanbul ignore next */
   message = `No valid \`ref\` provided.
-You shuld use \`ref\` from render prop of Item component.
+You should use \`ref\` from render prop of Item component.
 Example:
 <Item>{({ ref }) => <div ref={ref}></div>}</Item>\n`
 }
