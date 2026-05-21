7e8ffd035751b5e70d558a9d2c41c6705dcfb750
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2026-05-21 21:15:28 +0200

    refactor: remove prop-types runtime declarations
    TypeScript types in types.ts already provide full type safety.
    PropTypes runtime checks are redundant for TS consumers and
    unnecessary for the library's correctness.

--- src/item.ts
+++ src/item.ts
@@ -1,5 +1,4 @@
 import { useRef, useEffect, useMemo, useCallback } from 'react'
-import PropTypes from 'prop-types'
 import { ChildrenFnProps, ItemProps } from './types'
 import { useApiContext } from './hooks'
 import { NoRefError } from './no-ref-error'
@@ -54,18 +53,3 @@ export const Item = <NodeType extends HTMLElement>({
   return children(childrenFnProps)
 }
 
-Item.propTypes = {
-  children: PropTypes.func.isRequired,
-  original: PropTypes.string,
-  originalSrcset: PropTypes.string,
-  thumbnail: PropTypes.string,
-  width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-  height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-  alt: PropTypes.string,
-  caption: PropTypes.string,
-  content: PropTypes.element,
-  html: PropTypes.string,
-  id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-  cropped: PropTypes.bool,
-  sourceId: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-}
