16744f4df34fc581da8033b27def7f65b0fef676
zigzagdev <msts.oo0131@gmail.com>
2026-07-02 15:17:53 +0900

    test(hash-to-object): add tests for URI decoding and equals sign handling

--- src/__tests__/hash-to-object.test.ts
+++ src/__tests__/hash-to-object.test.ts
@@ -1,12 +1,12 @@
 import hashToObject from '../helpers/hash-to-object'
 
-type ExpectedObject = Record<string, string | undefined>
+type ExpectedObject = Record<string, string>
 
 describe('hashToObject helper', () => {
   test('single key, without value', () => {
     const hash = 'my-block'
     const expectedObject: ExpectedObject = {
-      'my-block': undefined,
+      'my-block': '',
     }
     expect(hashToObject(hash)).toEqual(expectedObject)
   })
@@ -40,9 +40,36 @@ describe('hashToObject helper', () => {
   test('complex', () => {
     const hash = 'my-block&gid=my-gallery&pid=second-slide'
     const expectedObject: ExpectedObject = {
-      'my-block': undefined,
+      'my-block': '',
       gid: 'my-gallery',
       pid: 'second-slide',
+    }
+    expect(hashToObject(hash)).toEqual(expectedObject)
+  })
+
+  test('should decode URI-encoded keys and values', () => {
+    const hash = 'gid=my%2Fgallery&pid=photo%2F1'
+    const expectedObject: ExpectedObject = {
+      gid: 'my/gallery',
+      pid: 'photo/1',
+    }
+    expect(hashToObject(hash)).toEqual(expectedObject)
+  })
+
+  test('should decode encoded special characters', () => {
+    const hash = 'gid=my%20gallery&pid=slide%231'
+    const expectedObject: ExpectedObject = {
+      gid: 'my gallery',
+      pid: 'slide#1',
+    }
+    expect(hashToObject(hash)).toEqual(expectedObject)
+  })
+
+  test('should handle value containing equals sign', () => {
+    const hash = 'gid=my-gallery&data=key%3Dvalue'
+    const expectedObject: ExpectedObject = {
+      gid: 'my-gallery',
+      data: 'key=value',
     }
     expect(hashToObject(hash)).toEqual(expectedObject)
   })
